16e7d6b62e4992cd1ab144263e1266bf71a27c11
E1\p.alekseyev <p.alekseyev@epc-it.com>
2025-05-27 05:02:59 +0300

    *[CV] - updated Skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import SkillLevelRow from '../../common/components/SkillLevelRow';
 import { SKILLS } from '../../common/constants/skillsAndLanguages';
-import './styles.scss';
 
 const Skills = () => {
   return (
-    <section className='section-container data-HILT'>
+    <section className='section-container'>
       <header className='heading'>
         <span className='sectiontitle'>Skills</span>
       </header>
